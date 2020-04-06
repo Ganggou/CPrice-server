b49f0e3ec8919dc879eab00d71791bd9c18e565e
xg <xg@paiyou.co>
2020-04-06 17:26:53 +0800

    larger interval

--- bin/gen_cronjobs.rb
+++ bin/gen_cronjobs.rb
@@ -1,6 +1,6 @@
 puts [
-  "*/10 * * * * Good.generate_record",
-  "*/10 * * * * Task.check"
+  "1 * * * * Good.generate_record",
+  "1 * * * * Task.check"
 ].map { |job|
   *schedule, cmd = job.split(' ')
   "#{schedule.join(" ")} cd /usr/src/app; rails runner '#{cmd}'"

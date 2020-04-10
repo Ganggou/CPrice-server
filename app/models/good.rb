f860d8dcdb32af525b57d660578490a63e3f16d0
xg <xg@paiyou.co>
2020-04-10 23:18:43 +0800

    try 3 times

--- app/models/good.rb
+++ app/models/good.rb
@@ -10,18 +10,21 @@ class Good < ApplicationRecord
     Good.all.each do |g|
       p = Platform.find_by_id(g.platform_id)
       if p.present?
-        begin
-          Timeout::timeout(15) {
-            socket.write("colly #{p.code} #{g.short_id}")
-            price = socket.readline
-            if price.present? && !price.include?('err')
-              g.price = price.to_i
-              g.save
-              g.records.create(price: price.to_i)
-            end
-          }
-        rescue
-        end
+        3.times {
+          begin
+            Timeout::timeout(15) {
+              socket.write("colly #{p.code} #{g.short_id}")
+              price = socket.readline
+              if price.present? && !price.include?('err')
+                g.price = price.to_i
+                g.save
+                g.records.create(price: price.to_i)
+              end
+              break
+            }
+          rescue
+          end
+        }
       end
     end
     socket.close

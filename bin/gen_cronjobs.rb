947cfc10881aa6dc2dd2a6e02809926c33de9e08
xg <xg@paiyou.co>
2020-04-11 18:08:58 +0800

    refine crontab

--- bin/gen_cronjobs.rb
+++ bin/gen_cronjobs.rb
@@ -1,6 +1,6 @@
 puts [
   "10 * * * * Good.generate_record",
-  "10 * * * * Task.check"
+  "13 * * * * Task.check"
 ].map { |job|
   *schedule, cmd = job.split(' ')
   "#{schedule.join(" ")} cd /usr/src/app; rails runner '#{cmd}'"

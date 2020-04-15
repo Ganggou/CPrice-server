321a1dc65a74ef3e1da38884c5e8b8de160b5d51
xg <xg@paiyou.co>
2020-04-15 10:45:38 +0800

    generate record retry

--- app/models/good.rb
+++ app/models/good.rb
@@ -8,20 +8,26 @@ class Good < ApplicationRecord
     Good.all.each do |g|
       p = Platform.find_by_id(g.platform_id)
       if p.present?
-        begin
-          Timeout::timeout(15) {
-            request = "#{$redis.get('colly_url')}/colly?platform_id=#{p.code}&short_id=#{g.short_id}"
-            body = HTTParty.post(request).body
-            data= JSON.parse(body)
-            price = data['price']
-            if price.present? && !price.to_i.zero?
-              g.price = price.to_i
-              g.save
-              g.records.create(price: price.to_i)
-            end
+        to_break = false
+        3.times do
+          begin
+            Timeout::timeout(15) {
+              request = "#{$redis.get('colly_url')}/colly?platform_id=#{p.code}&short_id=#{g.short_id}"
+              body = HTTParty.post(request).body
+              data= JSON.parse(body)
+              price = data['price']
+              if price.present? && !price.to_i.zero?
+                g.price = price.to_i
+                g.save
+                g.records.create(price: price.to_i)
+                to_break = true
+              end
+            }
+          rescue
+          end
+          if to_break
             break
-          }
-        rescue
+          end
         end
       end
     end
